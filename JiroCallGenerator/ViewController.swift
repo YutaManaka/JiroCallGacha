385dadb976a4119d951c8b049cf5db7f3a70aa4b
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2021-07-23 10:51:15 +0900

    Initial Commit

---
+++ JiroCallGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JiroCallGenerator
+//
+//  Created by 勇太_物販 on 2021/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
