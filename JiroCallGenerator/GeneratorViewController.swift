ec26a90f79a5dc42833f105071680d97f2ec0645
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2021-08-11 23:49:39 +0900

    [add] GeneratorViewにボタンとテキストフィールドを設置

--- JiroCallGenerator/GeneratorViewController.swift
+++ JiroCallGenerator/GeneratorViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class GeneratorViewController: UIViewController {
-    override func viewDidLoad() {
-            super.viewDidLoad()
-        }
-    
+    //ボタンの宣言
+    @IBOutlet weak var generatorButton: UIButton!
+    @IBOutlet weak var GachaBtn: UIButton!
+   
+    //遷移ボタン
     @IBAction func tapGachaBtn(_ sender: Any) {
         // Storyboardのインスタンスを名前指定で取得する
         let storyboard = UIStoryboard(name: "GachaView", bundle: nil)
@@ -22,4 +23,26 @@ class GeneratorViewController: UIViewController {
         // presentする
         self.present(nextVC, animated: true, completion: nil)
     }
+    
+    // ボタンを押したときの処理
+    // TODO https://www.zinbloger.com/2021/05/swiftpickerview2/
+    // TODO2 4つのtextfieldの位置をレスポンシブにする
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // ボタンの装飾
+        let buttonColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1.0)
+        generatorButton.backgroundColor = buttonColor
+        generatorButton.layer.borderWidth = 5
+        generatorButton.layer.borderColor = UIColor.red.cgColor
+        generatorButton.layer.cornerRadius = 5.0
+        GachaBtn.backgroundColor = buttonColor
+    //        GeneratorBtn.layer.borderWidth = 5
+    //        GeneratorBtn.layer.borderColor = UIColor.red.cgColor
+        GachaBtn.layer.cornerRadius = 5.0
+        let buttonTextColor = UIColor(red: 247/255, green: 229/255, blue: 4/255, alpha: 1.0)
+        GachaBtn.setTitleColor(buttonTextColor, for: UIControl.State.normal)
+        //背景色
+        view.backgroundColor = buttonTextColor
+        }
 }
